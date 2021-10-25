377251468b1d2418c1ebfcc22d3ced75117a5b84
eddiejpot <jamesedwardporter@gmail.com>
2021-10-25 13:58:43 +0800

    added page3

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 =========================================================== */
 const pageOneButton = document.querySelector("#button-page-1")
 const pageTwoButton = document.querySelector("#button-page-2")
+const pageThreeButton = document.querySelector("#button-page-3")
 const mainContainer = document.querySelector("#main-container")
